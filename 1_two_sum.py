dfc63722ca8176fea785111ea3fd2a98dbfc7bdd
zhangruochi <zrc720@gmail.com>
2017-08-08 23:44:17 +0800

    2017-08-08

--- 1_two_sum.py
+++ 1_two_sum.py
@@ -19,20 +19,33 @@ return [0, 1].
 
 
 class Solution:
-    def twoSum(self, nums, target):
+    # 最简单但时间复杂度没有通过
+    def twoSum_1(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
         """
-        for index_1, first in enumerate(nums):
-            for index_2,second in enumerate(nums):
-                if first
-            
+        for i in range(len(nums) - 1):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+        return None
+
+    def twoSum(self, nums, target):
+        # 以空间换时间
+        table = {}
+
+        for i in range(len(nums)):
+            remain = target - nums[i]
+            if remain in table:
+                return table[remain], i
+            table[nums[i]] = i
 
         return None
 
 
 if __name__ == '__main__':
     solution = Solution()
-    print(solution.twoSum([3,3],9))       
+    print(solution.twoSum([3,2,4], 6))
