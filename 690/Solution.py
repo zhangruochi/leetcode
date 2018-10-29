e72e3f5a17f2b5b9677f79cbd9233696926a6489
zhangruochi <zrc720@gmail.com>
2018-10-29 00:22:53 -0400

    2018-10-29

---
+++ 690/Solution.py
@@ -0,0 +1,52 @@
+"""
+You are given a data structure of employee information, which includes the employee's unique id, his importance value and his direct subordinates' id.
+
+For example, employee 1 is the leader of employee 2, and employee 2 is the leader of employee 3. They have importance value 15, 10 and 5, respectively. Then employee 1 has a data structure like [1, 15, [2]], and employee 2 has [2, 10, [3]], and employee 3 has [3, 5, []]. Note that although employee 3 is also a subordinate of employee 1, the relationship is not direct.
+
+Now given the employee information of a company, and an employee id, you need to return the total importance value of this employee and all his subordinates.
+
+Example 1:
+
+Input: [[1, 5, [2, 3]], [2, 3, []], [3, 3, []]], 1
+Output: 11
+Explanation:
+Employee 1 has importance value 5, and he has two direct subordinates: employee 2 and employee 3. They both have importance value 3. So the total importance value of employee 1 is 5 + 3 + 3 = 11.
+"""
+
+"""
+# Employee info
+class Employee:
+    def __init__(self, id, importance, subordinates):
+        # It's the unique id of each node.
+        # unique id of this employee
+        self.id = id
+        # the importance value of this employee
+        self.importance = importance
+        # the id of direct subordinates
+        self.subordinates = subordinates
+"""
+class Solution:
+    def getImportance(self, employees, id):
+        """
+        :type employees: Employee
+        :type id: int
+        :rtype: int
+        """
+        employee_table = {}
+        stack = []
+        for employee in employees:
+            employee_table[employee.id] = employee
+        
+        stack = [employee_table[id]]
+        total = 0
+        visited = set([id])
+        
+        while stack:
+            employee = stack.pop()
+            total += employee.importance
+            for sub in employee.subordinates:
+                if sub not in visited:
+                    visited.add(sub)
+                    stack.append(employee_table[sub])
+        
+        return total
--- README.md
+++ README.md
@@ -165,6 +165,9 @@ Table of Contents
 |No.112|Path Sum|[Solution](./112/Solution.py)|[Note](./112/note.md)|Easy|O(n)|O(1)|DFS|
 |No.257|Binary Tree Paths|[Solution](./257/Solution.py)|[Note](./257/note.md)|Easy|O(n)|O(1)|DFS|
 |No.257|Nested List Weight Sum|[Solution](./257/Solution.py)|[Note](./257/note.md)|Easy|O(n)|O(1)|DFS|
+|No.690|Employee Importance|[Solution](./690/Solution.py)|[Note](./690/note.md)|Easy|O(n)|O(n)|DFS|
+
+
 
 ## Hash Table
 |NO.|Title|Solution|Note|Difficulty|Time|Space|Tag|
