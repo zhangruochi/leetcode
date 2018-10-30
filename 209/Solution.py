71cbf9613918550c5619573847c5407bda3f0da1
zhangruochi <zrc720@gmail.com>
2018-10-30 12:54:12 -0400

    2018-10-30

--- 209/Solution.py
+++ 209/Solution.py
@@ -10,6 +10,7 @@ Follow up:
 If you have figured out the O(n) solution, try coding another solution of which the time complexity is O(n log n). 
 """
 
+## time complexity O(n) 
 class Solution:
     def minSubArrayLen(self, s, nums):
         """
@@ -36,3 +37,45 @@ class Solution:
                
             
         return count_
+
+
+## time complexity O(nlogn)
+class Solution:
+    
+    ## 查找第一个大于 key 的元素
+    def binarySearch(self,nums,key,low,high):
+        if high < low:
+            return -1
+            
+        mid = low + ((high-low) >> 1)
+        if nums[mid] < key:
+            return self.binarySearch(nums,key,mid+1,high)
+        else:
+            if mid == 0 or nums[mid-1] < key:
+                return mid
+            else:
+                return self.binarySearch(nums,key,low,mid-1)
+        
+        
+    def minSubArrayLen(self, s, nums):
+        """
+        :type s: int
+        :type nums: List[int]
+        :rtype: int
+        """
+        if sum(nums) < s:
+            return 0
+        
+        prefix_sums,cum = [0],0
+        for num in nums:
+            cum += num
+            prefix_sums.append(cum)
+        print(prefix_sums)
+        length = float("inf")
+        for i,prefix_sum in enumerate(prefix_sums):
+            j = self.binarySearch(prefix_sums,prefix_sum+s,0,len(prefix_sums)-1)
+            if j != -1:
+                length = min(length,j-i)
+        
+        return length
+            
