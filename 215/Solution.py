6010f5a521917e0844c992a8e989f51d1617ea71
zhangruochi <zrc720@gmail.com>
2020-01-30 23:01:14 -0500

    2020-01-30

--- 215/Solution.py
+++ 215/Solution.py
@@ -53,4 +53,31 @@ class Solution:
         
         return heapq.heappop(heap)
         
+import heapq
+
+
+
+def quick_sort(array, k):
+
+    mid = len(array) // 2
+    left_array = [num for num in array if num > array[mid]]
+    right_array = [num for num in array if num < array[mid]]
+    
+    left_array += [array[mid]] * (len(array) - len(left_array) - len(right_array) - 1)
+
+
+    if len(left_array) == k:
+        return array[mid]
+
+    elif len(left_array) >= k+1:
+        return quick_sort(left_array, k)
+    else:
+        return quick_sort(right_array, k - len(left_array) - 1)
+    
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        return quick_sort(nums,k-1)
+        
+        
         
