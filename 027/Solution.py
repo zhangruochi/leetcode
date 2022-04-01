edff9a5ca34ce1e0dc5b1b3dd75678bc4e937690
zhangruochi <zrc720@gmail.com>
2022-04-02 00:42:38 +0800

    20220402

--- 027/Solution.py
+++ 027/Solution.py
@@ -52,6 +52,17 @@ class Solution:
                 index += 1
         return index
 
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        index = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[index] = nums[i]
+                index += 1
+        return index
+
+
 if __name__ == '__main__':
     print(Solution().removeElement([0,1,2,2,3,0,4,2],2))
 
