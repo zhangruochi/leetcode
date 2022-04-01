2279f05b5cf872e214d7707222306c53ed3d750a
zhangruochi <zrc720@gmail.com>
2022-04-02 00:37:07 +0800

    20220402

--- 146/Solution.py
+++ 146/Solution.py
@@ -96,6 +96,36 @@ class LRUCache:
 
         
 
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        from collections import deque, OrderedDict
+
+        self.cache = OrderedDict()
+        self.capacity = capacity
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+
+        self.cache.move_to_end(key, last=True)
+        return self.cache[key]
+
+    def put(self, key: int, value: int) -> None:
+        self.cache[key] = value
+        self.cache.move_to_end(key, last=True)
+
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last=False)
+        
+        
+
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
