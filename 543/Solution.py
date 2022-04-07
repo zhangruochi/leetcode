88b1473d153bd4c0b1c671ebf71051c990830150
zhangruochi <zrc720@gmail.com>
2022-04-07 23:32:51 +0800

    20220407

--- 543/Solution.py
+++ 543/Solution.py
@@ -57,5 +57,37 @@ class Solution:
         helper(root)
         
         return res
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def diameterOfBinaryTree(self, root: TreeNode) -> int:
+
+        if not root:
+            return 0
+
+        res = 0
+
+        def helper(root):
+            nonlocal res
+
+            if not root:
+                return 0
+
+            left = helper(root.left)
+            right = helper(root.right)
+
+            res = max(res, left + right)
+
+            return max(left, right) + 1
+
+        helper(root)
+
+        return res
             
         
--- LZOF_26/Solution.py
+++ LZOF_26/Solution.py
@@ -3,21 +3,6 @@
 
 B是A的子结构， 即 A中有出现和B相同的结构和节点值。
 
-例如:
-给定的树 A:
-
-     3
-    / \
-   4   5
-  / \
- 1   2
-给定的树 B：
-
-   4 
-  /
- 1
-返回 true，因为 B 与 A 的一个子树拥有相同的结构和节点值。
-
 示例 1：
 
 输入：A = [1,2,3], B = [3,1]
@@ -29,10 +14,6 @@ B是A的子结构， 即 A中有出现和B相同的结构和节点值。
 限制：
 
 0 <= 节点个数 <= 10000
-
-来源：力扣（LeetCode）
-链接：https://leetcode-cn.com/problems/shu-de-zi-jie-gou-lcof
-著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 """
 
 # Definition for a binary tree node.
@@ -79,3 +60,30 @@ class Solution:
         pre_order(A,B)
 
         return res
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+def is_same(root_a, root_b):
+    if not root_a and not root_b:
+        return True
+    elif not root_b and root_a:
+        return True
+    elif not root_a and root_b:
+        return False
+    else:
+        return root_a.val == root_b.val and is_same(root_a.left, root_b.left) and is_same(root_a.right, root_b.right)
+                
+class Solution:
+    def isSubStructure(self, A: TreeNode, B: TreeNode) -> bool:
+
+        if not A or not B:
+            return False
+        else:
+            return is_same(A, B) or self.isSubStructure(A.left, B) or self.isSubStructure(A.right, B) 
+            
