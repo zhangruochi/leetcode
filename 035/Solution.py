0e6b4d75f4b8d66c620d1f5ade93ae32ec9a0f61
zhangruochi <zrc720@gmail.com>
2022-04-23 00:11:14 +0800

    20220423

---
+++ 035/Solution.py
@@ -0,0 +1,50 @@
+"""
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/search-insert-position
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        
+
+        def helper(l, r):
+            if l > r:
+                return l
+
+            mid = (l + r) // 2
+
+            if nums[mid] > target:
+                return helper(l, mid-1)
+            else:
+                if nums[mid] == target:
+                    return mid
+                    
+                elif (mid < len(nums)-2 and nums[mid] < target and nums[mid+1] > target):
+                    return mid + 1
+                else:
+                    return helper(mid+1, r)
+        
+        return helper(0, len(nums)-1)
+
+
+                
